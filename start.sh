b2b1998e15a38329dbd27264675e7774c73678e7
Sathya3088 <shanow@hotmail.fr>
2025-03-13 13:30:06 +0100

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Mise à jour des paquets
+apt-get update
+
+# Installation des dépendances
+apt-get -y install curl
+apt-get -y install python3-distutils
+Sandirin_Sathya_1_dashboard_022025.py
